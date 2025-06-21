05f35958de1c2a1ff4b9c2433e0105104ca2713e
a.prieto <alexandreprietobr@gmail.com>
2025-06-21 15:34:58 -0300

    inclusão de método para listar pessoas e ajuste do método de inclusão.

--- src/main/java/br/com/g4f/teste/alexandre/cadastroAPI/controller/PessoaController.java
+++ src/main/java/br/com/g4f/teste/alexandre/cadastroAPI/controller/PessoaController.java
@@ -1,19 +1,29 @@
 package br.com.g4f.teste.alexandre.cadastroAPI.controller;
 
 import br.com.g4f.teste.alexandre.cadastroAPI.pessoa.DadosCadastroPessoa;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import br.com.g4f.teste.alexandre.cadastroAPI.pessoa.Pessoa;
+import br.com.g4f.teste.alexandre.cadastroAPI.repository.PessoaRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/pessoa")
 
 public class PessoaController {
+    @Autowired
+    private PessoaRepository pessoaRepository;
 
     @PostMapping
-    public void cadastrar (@RequestBody DadosCadastroPessoa json){
-        System.out.println(json);
+    public Pessoa cadastrarPessoa (@RequestBody Pessoa pessoa){
+        return pessoaRepository.save(pessoa);
 
     }
+
+    @GetMapping
+    public List<Pessoa> listaPessoas(){
+        return pessoaRepository.findAll();
+    }
+
 }
