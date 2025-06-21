e29b1139cdfec3eb39c5d13758f84cfd82d9e062
a.prieto <alexandreprietobr@gmail.com>
2025-06-21 15:24:06 -0300

    Criação da interface PessoaRepository

---
+++ src/main/java/br/com/g4f/teste/alexandre/cadastroAPI/repository/PessoaRepository.java
@@ -0,0 +1,6 @@
+package br.com.g4f.teste.alexandre.cadastroAPI.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PessoaRepository extends JpaRepository {
+}
