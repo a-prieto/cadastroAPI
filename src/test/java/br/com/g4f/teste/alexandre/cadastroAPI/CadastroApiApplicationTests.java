47d6dc0378c84d5a432238191165a9d7923159c2
a.prieto <alexandreprietobr@gmail.com>
2025-06-21 14:54:33 -0300

    commit inicial

---
+++ src/test/java/br/com/g4f/teste/alexandre/cadastroAPI/CadastroApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.g4f.teste.alexandre.cadastroAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CadastroApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
