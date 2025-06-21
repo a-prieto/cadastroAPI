c98222a9ba9c93ec00f08bb7bbb14d263298da82
a.prieto <alexandreprietobr@gmail.com>
2025-06-21 15:04:12 -0300

    criação classe pessoa com getters

---
+++ src/main/java/br/com/g4f/teste/alexandre/cadastroAPI/pessoa/Pessoa.java
@@ -0,0 +1,30 @@
+package br.com.g4f.teste.alexandre.cadastroAPI.pessoa;
+
+public class Pessoa {
+
+    private String nome;
+    private String cpf;
+    private String dia;
+    private String mes;
+    private String ano;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public String getDia() {
+        return dia;
+    }
+
+    public String getMes() {
+        return mes;
+    }
+
+    public String getAno() {
+        return ano;
+    }
+}
